18656718913e882a122d659bc0725202cb6a14ef
Stuart Sierra <mail@stuartsierra.com>
2012-05-16 17:36:19 -0400

    TNS-2: Add functions to parse dependencies from ns declaration
    This code was originally written for Lazytest:
    https://github.com/stuartsierra/lazytest
    
    I, Stuart Sierra, give my permission to release this code under the
    Clojure Contributor Agreement. I am the sole author of this code.

--- src/main/clojure/clojure/tools/namespace.clj
+++ src/main/clojure/clojure/tools/namespace.clj
@@ -134,3 +134,30 @@
   of the symbol names of the declared namespaces."
   []
   (map second (find-ns-decls-on-classpath)))
+
+
+;;; Parsing dependencies
+
+(defn- deps-from-libspec [prefix form]
+  (cond (list? form) (apply union (map (fn [f] (deps-from-libspec
+						(symbol (str (when prefix (str prefix "."))
+							     (first form)))
+						f))
+				       (rest form)))
+	(vector? form) (deps-from-libspec prefix (first form))
+	(symbol? form) #{(symbol (str (when prefix (str prefix ".")) form))}
+	(keyword? form) #{}
+	:else (throw (IllegalArgumentException.
+		      (pr-str "Unparsable namespace form:" form)))))
+
+(defn- deps-from-ns-form [form]
+  (when (and (list? form)
+	     (contains? #{:use :require} (first form)))
+    (apply union (map #(deps-from-libspec nil %) (rest form)))))
+
+(defn deps-from-ns-decl
+  "Given a (quoted) ns declaration, returns a set of symbols naming
+  the dependencies of that namespace.  Handles :use and :require
+  clauses but not :load."
+  [decl]
+  (apply union (map deps-from-ns-form decl)))
