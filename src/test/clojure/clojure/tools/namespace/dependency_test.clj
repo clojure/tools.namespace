b103afcebcdefd816bfbf63c56f018aa496c3847
Stuart Sierra <mail@stuartsierra.com>
2012-08-03 13:58:24 -0400

    Make real tests out of documentation comments in 'dependency'

---
+++ src/test/clojure/clojure/tools/namespace/dependency_test.clj
@@ -0,0 +1,64 @@
+(ns clojure.tools.namespace.dependency-test
+  (:use clojure.test
+        clojure.tools.namespace.dependency))
+
+;; building a graph like:
+;;
+;;       :a
+;;      / |
+;;    :b  |
+;;      \ |
+;;       :c
+;;        |
+;;       :d
+;;
+(def g1 (-> (graph)
+            (depend :b :a)   ; "B depends on A"
+            (depend :c :b)   ; "C depends on B"
+            (depend :c :a)   ; "C depends on A"
+            (depend :d :c))) ; "D depends on C"
+
+;;      'one    'five
+;;        |       |
+;;      'two      |
+;;       / \      |
+;;      /   \     |
+;;     /     \   /
+;; 'three   'four
+;;    |      /
+;;  'six    /
+;;    |    /
+;;    |   /
+;;    |  /
+;;  'seven
+;;
+(def g2 (-> (graph)
+            (depend 'two   'one)
+            (depend 'three 'two)
+            (depend 'four  'two)
+            (depend 'four  'five)
+            (depend 'six   'three)
+            (depend 'seven 'six)
+            (depend 'seven 'four)))
+
+(deftest t-transitive-dependencies
+  (is (= #{:a :c :b}
+         (transitive-dependencies g1 :d)))
+  (is (= '#{two four six one five three}
+         (transitive-dependencies g2 'seven))))
+
+(deftest t-transitive-dependents
+  (is (= '#{four seven}
+         (transitive-dependents g2 'five)))
+  (is (= '#{four seven six three}
+         (transitive-dependents g2 'two))))
+
+(deftest t-topo-comparator
+  (is (= '(:a :b :d :foo)
+         (sort (topo-comparator g1) [:d :a :b :foo])))
+  (is (= '(three five seven nine eight)
+         (sort (topo-comparator g2) '[three seven nine eight five]))))
+
+(deftest t-topo-sort
+  (is (= '(one two three five six four seven)
+         (topo-sort g2))))
