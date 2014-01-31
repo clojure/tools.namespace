c85c4be60cb2f16bfdbaa2b2be4a7d6eafd5a091
Stuart Sierra <mail@stuartsierra.com>
2014-01-31 09:58:21 -0500

    TNS-16: do not test fixed order in topological sort tests
    There are many possible topological sort orders. The exact order
    returned depends on Clojure's hashing functions, which will change in
    Clojure 1.6.0.

--- src/test/clojure/clojure/tools/namespace/dependency_test.clj
+++ src/test/clojure/clojure/tools/namespace/dependency_test.clj
@@ -53,15 +53,60 @@
   (is (= '#{four seven six three}
          (transitive-dependents g2 'two))))
 
-(deftest t-topo-comparator
-  (is (= '(:a :b :d :foo)
-         (sort (topo-comparator g1) [:d :a :b :foo])))
-  (is (= '(three five seven nine eight)
-         (sort (topo-comparator g2) '[three seven nine eight five]))))
+(defn- before?
+  "True if x comes before y in an ordered collection."
+  [coll x y]
+  (loop [[item & more] (seq coll)]
+    (cond (nil? item) true  ; end of the seq
+          (= x item) true  ; x comes first
+          (= y item) false
+          :else (recur more))))
+
+(deftest t-before
+  (is (true? (before? [:a :b :c :d] :a :b)))
+  (is (true? (before? [:a :b :c :d] :b :c)))
+  (is (false? (before? [:a :b :c :d] :d :c)))
+  (is (false? (before? [:a :b :c :d] :c :a))))
+
+(deftest t-topo-comparator-1
+  (let [sorted (sort (topo-comparator g1) [:d :a :b :foo])]
+    (are [x y] (before? sorted x y)
+         :a :b
+         :a :d
+         :a :foo
+         :b :d
+         :b :foo
+         :d :foo)))
+
+(deftest t-topo-comparator-2
+  (let [sorted (sort (topo-comparator g2) '[three seven nine eight five])]
+    (are [x y] (before? sorted x y)
+         'three 'seven
+         'three 'eight
+         'three 'nine
+         'five  'eight
+         'five  'nine
+         'seven 'eight
+         'seven 'nine)))
 
 (deftest t-topo-sort
-  (is (= '(one two three five six four seven)
-         (topo-sort g2))))
+  (let [sorted (topo-sort g2)]
+    (are [x y] (before? sorted x y)
+         'one   'two
+         'one   'three
+         'one   'four
+         'one   'six
+         'one   'seven
+         'two   'three
+         'two   'four
+         'two   'six
+         'two   'seven
+         'three 'six
+         'three 'seven
+         'four  'seven
+         'five  'four
+         'five  'seven
+         'six   'seven)))
 
 (deftest t-no-cycles
   (is (thrown? Exception
