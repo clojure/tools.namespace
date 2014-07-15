ddf31606983282b44ddba18ee707deaf0cce4af9
Stuart Sierra <mail@stuartsierra.com>
2014-07-15 19:16:20 -0400

    Comment describing shape of the tracker map

--- src/main/clojure/clojure/tools/namespace/track.clj
+++ src/main/clojure/clojure/tools/namespace/track.clj
@@ -108,3 +108,35 @@
   "Returns a new, empty dependency tracker"
   []
   {})
+
+(comment
+  ;; Structure of the namespace tracker map
+
+  {;; Dependency graph of namespace names (symbols) as defined in
+   ;; clojure.tools.namespace.dependency/graph
+   :clojure.tools.namespace.track/deps {}
+
+   ;; Ordered list of namespace names (symbols) that need to be
+   ;; removed to bring the running system into agreement with the
+   ;; source files.
+   :clojure.tools.namespace.track/unload ()
+
+   ;; Ordered list of namespace names (symbols) that need to be
+   ;; (re)loaded to bring the running system into agreement with the
+   ;; source files.
+   :clojure.tools.namespace.track/load ()
+
+   ;; Added by clojure.tools.namespace.file: Map from source files
+   ;; (java.io.File) to the names (symbols) of namespaces they
+   ;; represent.
+   :clojure.tools.namespace.file/filemap {}
+
+   ;; Added by clojure.tools.namespace.dir: Set of source files
+   ;; (java.io.File) which have been seen by this dependency tracker;
+   ;; used to determine when files have been deleted.
+   :clojure.tools.namespace.dir/files #{}
+
+   ;; Added by clojure.tools.namespace.dir: Instant when the
+   ;; directories were last scanned, as returned by
+   ;; System/currentTimeMillis.
+   :clojure.tools.namespace.dir/time 1405201862262})
