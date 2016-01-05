264825d6bafe31d78bfc87a2dfa52beb9ad122f2
Stuart Sierra <mail@stuartsierra.com>
2016-01-05 14:56:10 -0500

    Use helpers in c.t.n.move-test

--- src/test/clojure/clojure/tools/namespace/move_test.clj
+++ src/test/clojure/clojure/tools/namespace/move_test.clj
@@ -1,62 +1,42 @@
 (ns clojure.tools.namespace.move-test
-  (:use [clojure.java.io :as io]
-        [clojure.test :only (deftest is)]
-        [clojure.tools.namespace.move :only (move-ns)])
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [deftest is]]
+            [clojure.tools.namespace.move :refer [move-ns]]
+            [clojure.tools.namespace.test-helpers :as help])
   (:import (java.io File)))
 
-(defn- create-temp-dir
-  "Creates and returns a new temporary directory java.io.File."
-  [name]
-  (let [temp-file (File/createTempFile name nil)]
-    (.delete temp-file)
-    (.mkdirs temp-file)
-    temp-file))
+(defn- create-file-one [dir]
+  (help/create-source dir 'example.one :clj
+                      '[example.two example.three]
+                      '[(defn foo []
+                           (example.a.four/foo))]))
 
-(def ^:private file-one-contents "
- (ns com.example.one
-   (:require [com.example.two :as two]
-             [com.example.three :as three]))
+(defn- create-file-two [dir]
+  (help/create-source dir 'example.two :clj
+                    '[example.three example.a.four]))
 
- (defn foo []
-   (com.example.a.four/foo))
-")
+(defn- create-file-three [dir]
+  (help/create-source dir 'example.three :clj
+                    '[example.five]))
 
-(def ^:private file-two-contents "
- (ns com.example.two
-   (:require [com.example.three :as three]
-             [com.example.a.four :as four]))
-")
-
-(def ^:private file-three-contents "
- (ns com.example.three
-   (:use com.example.five))
-")
-
-(def ^:private file-four-contents "
- (ns com.example.a.four)
- (defn foo [] \"foo\")
-")
+(defn- create-file-four [dir]
+  (help/create-source dir 'example.a.four :clj))
 
 (deftest t-move-ns
-  (let [temp-dir (create-temp-dir "tools-namespace-t-move-ns")
+  (let [temp-dir (help/create-temp-dir "tools-namespace-t-move-ns")
         src-dir (io/file temp-dir "src")
-        example-dir (io/file temp-dir "src" "com" "example")
-        file-one (io/file example-dir "one.clj")
-        file-two (io/file example-dir "two.clj")
-        file-three (io/file example-dir "three.clj")
-        old-file-four (io/file example-dir "a" "four.clj")
+        example-dir (io/file temp-dir "src" "example")
+        file-one (create-file-one src-dir)
+        file-two (create-file-two src-dir)
+        file-three (create-file-three src-dir)
+        old-file-four (create-file-four src-dir)
         new-file-four (io/file example-dir "b" "four.clj")]
-
-    (.mkdirs (io/file example-dir "a"))
-    (spit file-one file-one-contents)
-    (spit file-two file-two-contents)
-    (spit file-three file-three-contents)
-    (spit old-file-four file-four-contents)
-    (Thread/sleep 1500) ;; ensure file timestamps are different
 
     (let [file-three-last-modified (.lastModified file-three)]
 
-      (move-ns 'com.example.a.four 'com.example.b.four src-dir [src-dir])
+      (Thread/sleep 1500) ;; ensure file timestamps are different
+
+      (move-ns 'example.a.four 'example.b.four src-dir [src-dir])
 
       (is (.exists new-file-four)
           "new file should exist")
@@ -66,7 +46,7 @@
           "old empty directory should not exist")
       (is (= file-three-last-modified (.lastModified file-three))
           "unaffected file should not have been modified")
-      (is (not-any? #(.contains (slurp %) "com.example.a.four")
+      (is (not-any? #(.contains (slurp %) "example.a.four")
                     [file-one file-two file-three new-file-four]))
-      (is (every? #(.contains (slurp %) "com.example.b.four")
+      (is (every? #(.contains (slurp %) "example.b.four")
                   [file-one file-two new-file-four])))))
